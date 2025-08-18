5da504d8ff4b4453eb0127ca6ea89b1e287ebeae
Bruno Pereira <bruno.pereira@sickkids.ca>
2025-08-18 12:51:14 -0400

    Separate partition and tenant headers

--- src/main/java/ca/uhn/fhir/jpa/starter/common/TenantIdentificationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/TenantIdentificationInterceptor.java
@@ -3,8 +3,10 @@ package ca.uhn.fhir.jpa.starter.common;
 import ca.uhn.fhir.interceptor.api.Hook;
 import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.interceptor.api.Pointcut;
+import ca.uhn.fhir.interceptor.model.RequestPartitionId;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -16,13 +18,15 @@ import org.springframework.stereotype.Component;
 public class TenantIdentificationInterceptor {
 
     private static final String X_PARTITION_NAME = "X-Partition-Name";
+    private static final String X_TENANT = "X-Tenant-Name";
 
     @Autowired
     private TenantProperties tenantProperties;
 
-    @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_PROCESSED)
-    public void preProcess(HttpServletRequest requestDetails, HttpServletResponse responseDetails) {
-        String tenantId = requestDetails.getHeader(X_PARTITION_NAME);
+    // @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_PROCESSED)
+    @Hook(Pointcut.STORAGE_PARTITION_IDENTIFY_ANY)
+    public RequestPartitionId preProcess(ServletRequestDetails requestDetails) {
+        String tenantId = requestDetails.getHeader(X_TENANT);
         
         if (tenantId == null || tenantId.trim().isEmpty()) {
             throw new InvalidRequestException("Missing required header: " + X_PARTITION_NAME);
@@ -31,12 +35,15 @@ public class TenantIdentificationInterceptor {
         if (!tenantProperties.getTenants().containsKey(tenantId)) {
             throw new InvalidRequestException("Invalid tenant ID: " + tenantId);
         }
-
+        System.err.println("Setting tenant ID to: " + tenantId);
         TenantContext.setTenantId(tenantId);
+        String partitionName = requestDetails.getHeader("X-Partition-Name");
+
+        return RequestPartitionId.fromPartitionName(partitionName);
     }
 
-    @Hook(Pointcut.SERVER_INCOMING_REQUEST_POST_PROCESSED)
-    public void postProcess(RequestDetails requestDetails) {
-        TenantContext.clear();
-    }
+//     @Hook(Pointcut.SERVER_INCOMING_REQUEST_POST_PROCESSED)
+//     public void postProcess(RequestDetails requestDetails) {
+//         TenantContext.clear();
+//     }
 }
